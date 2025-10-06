deb916bf197139cd8bfc3972d34e919d42eb1d5a
Muhammad Asim Salman <144407671+AsimS468@users.noreply.github.com>
2025-10-05 23:16:54 -0400

    Update CertificateCard.vue

--- src/components/cards/CertificateCard.vue
+++ src/components/cards/CertificateCard.vue
@@ -21,7 +21,7 @@ defineProps({
 
             <!-- Bar (always visible) -->
             <div class=" w-full justify-center items-center">
-                <a :href="CertificateLink" class="btn transition-[0.2s] absolute bottom-0 w-[200px] h-[75px] bg-[#2a2a2a] border-[#3f3f3f] border-[3px] flex items-center justify-center rounded-[30px] mb-[15px] text-white z-20 left-1/2 -translate-x-1/2">
+                <a :href="CertificateLink" target="_blank" class="btn transition-[0.2s] absolute bottom-0 w-[200px] h-[75px] bg-[#2a2a2a] border-[#3f3f3f] border-[3px] flex items-center justify-center rounded-[30px] mb-[15px] text-white z-20 left-1/2 -translate-x-1/2">
                     <span class="font-[altone-bold] text-2xl">See More →</span>
                 </a>
             </div>
